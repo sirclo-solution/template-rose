270c3993254065a0a3ab72c1902d74f93eae9aec
ardiankristo <40454642+ardiankristo@users.noreply.github.com>
2023-02-22 15:43:38 +0700

    Release-20230222 (#62)
    * feat(product-highlight): add product list page (#43)
    
    * feat(product-highlight): add product list page
    
    * refactor: remove unused codes
    
    * fix: use helper for thumbor width and product label title
    
    * feat(homepage): add product highlight and category (#44)
    
    * feat(homepage): add product highlight and category
    
    * update placeholder mapping
    
    * Merge pull request #47 from feliciakri/master
    
    chore: bump to 2.17.12, breaking changes and featureflag producthighlight
    
    * Merge pull request #60 from DiebrataA/fix/balikan-qa-ph-homepage
    
    Fix: balikan qa dan menerapkan tampilan productCard baru
    
    ---------
    
    Co-authored-by: Sultan Fariz <55394860+sultanfariz@users.noreply.github.com>
    Co-authored-by: Anggara Diebrata <38358288+DiebrataA@users.noreply.github.com>
    Co-authored-by: Felicia <feliciakrismanta@hotmail.com>
    Co-authored-by: Naofal Leo Agusta <naofal.leo@sirclo.com>

--- pages/[lng]/index.tsx
+++ pages/[lng]/index.tsx
@@ -5,8 +5,12 @@ import { useRouter } from 'next/router'
 import { LazyLoadComponent } from 'react-lazy-load-image-component'
 import { RiQuestionFill, RiStarFill } from 'react-icons/ri'
 import {
+  FeaturesType,
   getBanner,
   Products,
+  ProductCategory,
+  ProductHighlights,
+  TemplateFeatures,
   useAuthToken,
   useI18n,
   Widget
@@ -18,16 +22,17 @@ import { useBrand } from 'lib/useBrand'
 /* components */
 import Layout from 'components/Layout/Layout'
 import BannerComponent from 'components/Banner'
-import ProductTitle from 'components/ProductTitle'
 import Instafeed from 'components/Instafeed'
 import EmptyComponent from 'components/EmptyComponent/EmptyComponent'
 import Placeholder from 'components/Placeholder'
+import ProductTitle from 'components/ProductTitle'
 /* styles */
 import styleBanner from 'public/scss/components/Banner.module.scss'
 import styleWidget from 'public/scss/components/Widget.module.scss'
 import styleProduct from 'public/scss/components/Product.module.scss'
 import styleButton from 'public/scss/components/Button.module.scss'
 import stylePlaceholder from 'public/scss/components/Placeholder.module.scss'
+import styleCategory from  'public/scss/components/ProductCategory.module.scss'
 
 const classesProducts = {
   productContainerClassName: styleProduct.product_item,
@@ -47,6 +52,11 @@ const classesProducts = {
   salePriceContainerClassName: styleProduct.product_labelPrice__container,
   salePriceClassName: styleProduct.product_labelPrice__sale,
   priceClassName: styleProduct.product_labelPrice__price,
+  productSectionContainerClassName: styleProduct.product_highlight_sectionContainer,
+  productHighlightTitleContainerClassName: styleProduct.product_label__container,
+  productHighlightTitleClassName: styleProduct.product_highlight_title,
+  productHighlightSeeAllClassName: `${styleButton.btn} ${styleButton.btn_secondary}`,
+  productHighlightContainerClassName: styleProduct.product_highlight_container,
   
   //category
   categoryContainerClassName: styleProduct.product_category__container,
@@ -64,6 +74,12 @@ const classesCategoryProduct = {
   categoryItemsClassName: styleProduct.product_category__items,
 }
 
+const classesCategoryHome = {
+  parentCategoryClassName: styleCategory.category_home_parent,
+  categoryItemClassName: styleCategory.category_home_item,
+  categoryNameClassName: styleCategory.category_home_name
+}
+
 const classesPlaceholderProduct = {
   placeholderImage: `${stylePlaceholder.placeholderItem} ${stylePlaceholder.placeholderItem_productCard}`,
 }
@@ -79,8 +95,9 @@ const Home: FC<any> = ({
   dataBanners
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const i18n: any = useI18n()
-  const router: any = useRouter()
   const size = useWindowSize()
+
+  const router: any = useRouter()
   const [tagname, setTagname] = useState<string>('')
   const [totalItem, setTotalItem] = useState({
     ourproduct: null,
@@ -145,64 +162,122 @@ const Home: FC<any> = ({
           }}
         />
       </div>
-      <div className={styleProduct.product_container}>
-        <div className={`${styleProduct.product_containerItem} ${styleProduct.product_containerTitle}`}>
-          <ProductTitle
-            i18n={i18n}
-            styleProduct={styleProduct}
-            handleChangeTagname={handleChangeTagname}
-            tagnameActive={tagname}
-            totalProducts={totalItem}
-          />
-        </div>
-        <div className="container">
-          <div className={styleProduct.product_containerItem}>
-            <LazyLoadComponent>
-              <Products
-                tagName={tagname}
-                itemPerPage={8}
-                classes={classesProducts}
-                pathPrefix="product"
-                isFlipImage
-                withCategory
-                productCategoryClasses={classesCategoryProduct}
-                categoryLength={1}
-                withRating
-                showEmptyRating
-                ratingIcon={<RiStarFill color="#F2C14F" size={12} />}
-                lazyLoadedImage={false}
-                getPageInfo={(pageInfo) => getTotalItem(pageInfo.totalItems)}
-                emptyStateComponent={
-                  <EmptyComponent
-                    icon={<RiQuestionFill color="#A8A8A8" size={20} />}
-                    title={i18n.t("product.isEmpty")}
-                  />
-                }
-                thumborSetting={{
-                  width: size.width < 768 ? 512 : 800,
-                  format: "webp",
-                  quality: 85
-                }}
-                loadingComponent={
-                  <>
-                    <div><Placeholder classes={classesPlaceholderProduct} withImage /></div>
-                    <div><Placeholder classes={classesPlaceholderProduct} withImage /></div>
-                    <div><Placeholder classes={classesPlaceholderProduct} withImage /></div>
-                  </>
-                }
-              />
-            </LazyLoadComponent>
+      <TemplateFeatures
+        id={FeaturesType.PRODUCT_HIGHLIGHT}
+        defaultChildren={
+          <>
+            <div className={styleProduct.product_container}>
+              <div className={`${styleProduct.product_containerItem} ${styleProduct.product_containerTitle}`}>
+                <ProductTitle
+                  i18n={i18n}
+                  styleProduct={styleProduct}
+                  handleChangeTagname={handleChangeTagname}
+                  tagnameActive={tagname}
+                  totalProducts={totalItem}
+                />
+              </div>
+              <div className="container">
+                <div className={styleProduct.product_containerItem}>
+                  <LazyLoadComponent>
+                    <Products
+                      tagName={tagname}
+                      itemPerPage={8}
+                      classes={classesProducts}
+                      pathPrefix="product"
+                      isFlipImage
+                      productCategoryClasses={classesCategoryProduct}
+                      withRating
+                      showEmptyRating
+                      ratingIcon={<RiStarFill color="#F2C14F" size={12} />}
+                      lazyLoadedImage={false}
+                      getPageInfo={(pageInfo) => getTotalItem(pageInfo.totalItems)}
+                      emptyStateComponent={
+                        <EmptyComponent
+                          icon={<RiQuestionFill color="#A8A8A8" size={20} />}
+                          title={i18n.t("product.isEmpty")}
+                        />
+                      }
+                      thumborSetting={{
+                        width: size.width < 768 ? 512 : 800,
+                        format: "webp",
+                        quality: 85
+                      }}
+                      loadingComponent={
+                        <>
+                          <div><Placeholder classes={classesPlaceholderProduct} withImage /></div>
+                          <div><Placeholder classes={classesPlaceholderProduct} withImage /></div>
+                          <div><Placeholder classes={classesPlaceholderProduct} withImage /></div>
+                        </>
+                      }
+                    />
+                  </LazyLoadComponent>
+                </div>
+                <div className="text-center mt-4">
+                  <button
+                    className={`${styleButton.btn} ${styleButton.btn_secondary}`}
+                    onClick={() => router.push(`/${lng}/products`)}
+                  >
+                    {i18n.t("product.seeAll")}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </>
+        }
+      >
+        <div className={styleProduct.product_container}>
+          <div className={styleCategory.category_home_container}>
+            <div className={styleCategory.category_home_title_container}>
+              <h5 className={styleCategory.category_home_title}>{i18n.t("products.category")}</h5>
+            </div>
+            <ProductCategory
+              classes={classesCategoryHome}
+              showImages
+              isSelect
+              dropdownIcon={false}
+              widthImage={100}
+              heightImage={100}
+              thumborSetting={{
+                width: size.width < 768 ? 512 : 800,
+                format: "webp",
+                quality: 85
+              }}
+            />
           </div>
-          <div className="text-center mt-4">
-            <button
-              className={`${styleButton.btn} ${styleButton.btn_secondary}`}
-              onClick={() => router.push(`/${lng}/products`)}
-            >
-              {i18n.t("product.seeAll")}
-            </button>
+          <div className="container">
+            <div className={styleProduct.product_highlight}>
+              <LazyLoadComponent>
+                <ProductHighlights
+                  seeAllButtonPosition="Bottom"
+                  classes={classesProducts}
+                  item={4}
+                  sectionProductHighlight={'Display1'}
+                  pathPrefix="product"
+                  isFlipImage
+                  productCategoryClasses={classesCategoryProduct}
+                  withRating
+                  showEmptyRating
+                  ratingIcon={<RiStarFill color="#F2C14F" size={12} />}
+                  lazyLoadedImage={false}
+                  thumborSetting={{
+                    width: size.width < 768 ? 512 : 800,
+                    format: "webp",
+                    quality: 85
+                  }}
+                  loadingComponent={
+                    <div className={stylePlaceholder.placeholderItem_productContainer}>
+                      {[0,1,2].map((index)=>{
+                        return <div key={index}><Placeholder classes={classesPlaceholderProduct} withImage /></div>
+                      })}
+                    </div>
+                  }
+                />
+              </LazyLoadComponent>
+            </div>
           </div>
         </div>
-      </div>
+      </TemplateFeatures>
+
       <div className="container">
         <Widget
           pos="main-content-2"
@@ -222,6 +297,47 @@ const Home: FC<any> = ({
           }}
         />
       </div>
+      <TemplateFeatures
+        id={FeaturesType.PRODUCT_HIGHLIGHT}
+        defaultChildren={
+          <>
+          </>
+        }
+      >
+        <div className={styleProduct.product_container}>
+          <div className="container">
+              <div className={styleProduct.product_highlight}>
+                <LazyLoadComponent>
+                  <ProductHighlights
+                    seeAllButtonPosition="Bottom"
+                    classes={classesProducts}
+                    item={4}
+                    sectionProductHighlight={'Display2'}
+                    pathPrefix="product"
+                    isFlipImage
+                    productCategoryClasses={classesCategoryProduct}
+                    withRating
+                    showEmptyRating
+                    ratingIcon={<RiStarFill color="#F2C14F" size={12} />}
+                    lazyLoadedImage={false}
+                    thumborSetting={{
+                      width: size.width < 768 ? 512 : 800,
+                      format: "webp",
+                      quality: 85
+                    }}
+                    loadingComponent={
+                      <div className={stylePlaceholder.placeholderItem_productContainer}>
+                        {[0,1,2].map((index)=>{
+                          return <div key={index}><Placeholder classes={classesPlaceholderProduct} withImage /></div>
+                        })}
+                      </div>
+                    }
+                    />
+                </LazyLoadComponent>
+              </div>
+          </div>
+        </div>
+      </TemplateFeatures>
       {brand?.socmedSetting?.instagramToken &&
         <div className="container">
           <LazyLoadComponent>
