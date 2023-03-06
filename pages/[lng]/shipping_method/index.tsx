ea65fc72c895296374001a057e8b04206ca6631b
Virginia Ritonga <virginia.p.m@mail.ugm.ac.id>
2023-03-06 17:24:00 +0700

    fix(product-notes): adjust locales and hide cart notes on shipping method page

--- pages/[lng]/shipping_method/index.tsx
+++ pages/[lng]/shipping_method/index.tsx
@@ -11,8 +11,7 @@ import {
   ShippingMethods,
   useAuthToken,
   useI18n,
-  PrivateRoute,
-  useBuyerNotes
+  PrivateRoute
 } from '@sirclo/nexus'
 /* library template */
 import { useBrand } from 'lib/useBrand'
@@ -148,7 +147,6 @@ const ShippingMethodPage: FC<any> = ({
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const i18n: any = useI18n()
   const router: any = useRouter()
-  const { data: dataBuyerNotes } = useBuyerNotes()
 
   const layoutProps = {
     i18n,
@@ -209,9 +207,6 @@ const ShippingMethodPage: FC<any> = ({
               title={i18n.t("global.notes")}
               toDirect="cart"
             />
-            <div className={styles.customer_notes}>
-              {dataBuyerNotes?.buyerNotes?.buyerNotes || i18n.t("global.notesEmpty")}
-            </div>
           </div>
         </div>
         <div className={styles.shippingMethod}>
