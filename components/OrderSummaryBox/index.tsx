5a4ac96aba7fe4033684069ea21c11f08cba8176
Mutiara A'yun <mutiara.ayun19@gmail.com>
2023-05-09 11:26:18 +0700

    add mixin

--- components/OrderSummaryBox/index.tsx
+++ components/OrderSummaryBox/index.tsx
@@ -14,6 +14,7 @@ import {
   OrderSummary,
   CartDetails
 } from '@sirclo/nexus'
+/* components */
 import Loader from 'components/Loader/Loader'
 /* styles */
 import styleOrderSummary from 'public/scss/components/OrderSummaryBox.module.scss'
@@ -248,7 +249,7 @@ const OrderSummaryComponent: FC<iProps> = ({
               voucherRemoved: <FiX color="#CC4534" size={16} />,
               pointsApplied: <img src="/icons/point.svg" alt="points" />,
               copyIcon: <FaCopy />,
-							expand: <FiChevronUp color="#998060" size={16} stroke-width="3" />,
+							expand: <FiChevronUp color="#998060" size={16} stroke-width="3" />, 
 							collapse: <FiChevronDown color="#998060" size={16} stroke-width="3" />
             }}
           />
